e3636c23af5db504b255b31837c1732547e94e7c
Joshua Cox <mongst@gmail.com>
2019-03-24 14:57:30 -0400

    Add VIN: in activity2 appears

---
+++ app/src/main/java/com/mongst/vinlist2/CreateVin.java
@@ -0,0 +1,14 @@
+package com.mongst.vinlist2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class CreateVin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_vin);
+    }
+}
