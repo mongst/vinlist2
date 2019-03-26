067e7cb1af50f9f802321686e9942c14fd0f3b82
Joshua Cox <mongst@gmail.com>
2019-03-25 20:07:07 -0400

    Added database to Activity2
    need to edit

--- app/src/main/java/com/mongst/vinlist2/CreateVin.java
+++ app/src/main/java/com/mongst/vinlist2/CreateVin.java
@@ -1,14 +1,21 @@
 package com.mongst.vinlist2;
 
+import android.content.Intent;
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class CreateVin extends AppCompatActivity {
+
+    final static String TAG = CreateVin.class.getSimpleName();
 
     EditText createVin;
     EditText notesVin;
@@ -23,15 +30,16 @@ public class CreateVin extends AppCompatActivity {
         notesVin = findViewById(R.id.notes_vin);
         button = findViewById(R.id.button);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                Log.d(TAG, "onClick: createVin: " + createVin.getText().toString());
-                Vin vin = new Vin(createVin.getText().toString(), notesVin.getText().toString());
+        final AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "production")
+                .allowMainThreadQueries()
+                .build();
 
-            }
+        button.setOnClickListener((v) -> {
+            Log.d(TAG, "onClick: createVin: " + createVin.getText().toString());
 
-//            startActivity(new Intent(CreateVin.this, Activity2.class));
+            Vin vin = new Vin(createVin.getText().toString(), notesVin.getText().toString());
+            db.vinDao().insertAll(vin);
+            startActivity(new Intent(CreateVin.this, Activity2.class));
         });
     }
 }
